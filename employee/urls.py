89f027c6d01bc79b7bbf61b7f7556aed02d1f2aa
shilpasasidharan97 <shilpasasidharan97@gmail.com>
2022-07-30 16:52:22 +0530

    employtee

--- employee/urls.py
+++ employee/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 app_name = 'employee'
 
-urlpatterns = []
+urlpatterns = [
+    path('employeehome',views.employeeHome,name='employeehome'),
+
+    path('allemployees',views.allEmployees,name='allemployees'),
+    path('empdepartment',views.empDepartment,name='department'),
+    path('emptimeline',views.empTimeline,name='emptimeline'),
+
+
+    path('empteam',views.empTeam,name='empteam'),
+]
--- employee/views.py
+++ employee/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def employeeHome(request):
+    return render(request,'employee/home.html')
+
+
+# ----- employee ------
+def allEmployees(request):
+    return render(request,'employee/all_employees.html')
+
+def empDepartment(request):
+    return render(request,'employee/department.html')
+
+def empTimeline(request):
+    return render(request,'employee/timeline.html')
+# ------ team -------
+
+def empTeam(request):
+    return render(request,'employee/team.html')
