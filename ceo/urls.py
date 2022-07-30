cf13920893679854469a63a2f34f4c04d0897d48
shahilkk <shahilkhanarimbra@gmail.com>
2022-07-30 16:33:16 +0530

    added dashboard and others

--- ceo/urls.py
+++ ceo/urls.py
@@ -4,5 +4,17 @@ from . import views
 app_name = 'ceo'
 
 urlpatterns = [
-    path('base', views.base, name='base')
+    path('base', views.base, name='base'),
+    # dashboard
+    path('', views.admin, name='admin'),
+    path('crm', views.crm, name='crm'),
+    path('employee', views.employee, name='employee'),
+    path('hr', views.hr, name='hr'),
+    path('projectmanager', views.projectmanager, name='projectmanager'),
+    path('accounts', views.accounts, name='accounts'),
+    path('gm', views.gm, name='gm'),
+
+
+
+    path('employeeprofile', views.employeeprofile, name='employeeprofile'),
 ]
--- ceo/views.py
+++ ceo/views.py
@@ -5,3 +5,46 @@ from django.shortcuts import render
 
 def base(request):
     return render (request,'ceo/partials/base.html')
+
+
+def admin(request):
+    return render (request,'ceo/dashboard/admin.html')  
+
+
+def crm(request):
+    return render (request,'ceo/dashboard/crm.html')  
+
+
+def employee(request):
+    return render (request,'ceo/dashboard/employee.html')  
+
+
+def hr(request):
+    return render (request,'ceo/dashboard/hr.html')  
+
+
+
+def projectmanager(request):
+    return render (request,'ceo/dashboard/projectmanager.html')          
+
+
+def accounts(request):
+    return render (request,'ceo/dashboard/accounts.html')  
+
+
+def gm(request):
+    return render (request,'ceo/dashboard/gm.html')    
+
+
+
+def employeeprofile(request):
+    return render (request,'ceo/employeeprofile.html')    
+
+
+
+
+      
+
+
+
+    
