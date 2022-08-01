48d7ebf91b1f1fb39a47c84dd4749ffabe87bb87
mhdshahidak <mhdshd.ak@gmail.com>
2022-08-01 16:42:47 +0530

    side bar feachers completed

--- crm/urls.py
+++ crm/urls.py
@@ -4,6 +4,14 @@ from . import views
 app_name = 'crm'
 
 urlpatterns = [
-    path('',views.crmHome,name="crmhome"),
-    path('enquirylist',views.enquiryList,name="enquirylist"),
+    path('', views.crmHome, name="crmhome"),
+    path('enquirylist', views.enquiryList, name="enquirylist"),
+    path('clientlist', views.clientList, name="clientlist"),
+    path('projectlist', views.projectList, name="projectlist"),
+    path('followuplist', views.followUpList, name="followuplist"),
+    path('feedback', views.feedBack, name="feedback"),
+    path('leaves', views.leaves, name="leaves"),
+    path('attantance', views.attantance, name="attantance"),
+    path('settings', views.settings, name="settings"),
+    path('profile', views.profile, name="profile"),
 ]
