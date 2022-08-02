02397d3b25adc2f54dc95e12318b51b110170171
shahilkk <shahilkhanarimbra@gmail.com>
2022-08-02 17:03:45 +0530

    worked in pm side

--- pm/views.py
+++ pm/views.py
@@ -16,3 +16,49 @@ def enquiry(request):
 
 def viewenquries(request):
     return render (request,'pm/enquiry/viewenquries.html')     
+
+
+
+def proposal(request):
+    return render (request,'pm/proposal.html')     
+
+
+
+
+def project(request):
+    return render (request,'pm/project/project.html')    
+
+
+def projectlist(request):
+    return render (request,'pm/project/projectlist.html')  
+
+
+def viewproject(request):
+    return render (request,'pm/project/viewproject.html')  
+
+
+def unassigneproject(request):
+    return render (request,'pm/project/unassigneproject.html')         
+
+
+def addschedule(request):
+    return render (request,'pm/project/addschedule.html')    
+
+
+
+def task(request):
+    return render (request,'pm/project/task.html')
+    
+
+
+def viewtask(request):
+    return render (request,'pm/project/viewtask.html')  
+
+
+def dailyprogress(request):
+    return render (request,'pm/dailyprogress.html')    
+
+
+def viewdailyreport(request):
+    return render (request,'pm/viewdailyreport.html')  
+    
